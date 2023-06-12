51fac32f05e32fd497b9aa5876076a221a094b0d
Paulsams <pasha.samsonov234@gmail.com>
2023-06-13 02:04:34 +0300

    Add save tilemap algorithm

---
+++ Include/Utils/TMXGenerator.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <iostream>
+#include <zlib.h>
+#include "cocos2d.h"
+#include "Utils/Common.h"
+
+// The code from this repository is taken as a basis:
+// https://github.com/rohinnz/COCOS2D-X-TMXGenerator
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// base64.h
+std::string base64_encode(unsigned char const* , unsigned int len);
+// std::string base64_decode(std::string const& s);
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// TMXGenerator.h
+std::string generateXMLForTMXTiledMap(Tilemap* map);
