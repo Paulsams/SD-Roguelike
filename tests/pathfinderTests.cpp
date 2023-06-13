2fefce98c0d0c6032ec542bac53fa69cbe0680dc
AvvALlV <cghjhfdshj@gmail.com>
2023-06-13 23:48:33 +0300

    first test in pathfinder

--- tests/pathfinderTests.cpp
+++ tests/pathfinderTests.cpp
@@ -8,53 +8,53 @@ std::function<TileType(int)> fToTileType = [](int tile) {
     return static_cast<TileType>(tile);
 };
 
-//TEST(PathfinderSmokeTest, pathfinderSmokeTest) {
-//    const int height = 1, width = 5;
-//    const cocos2d::Size testSize(width, height);
-//    uint32_t tiles1[height * width] = {
-//            0, 0, 0, 0, 0,
-//    };
-//    pathfinder::Graph testGraph(testSize, tiles1, fToTileType);
-//
-//
-//    Vec2Int start(0, 0), end(4, 0);
-//    auto res = finder.findPath(testGraph, testGraph.getNodeByPos(start), testGraph.getNodeByPos(end));
-//
-//    std::vector<Vec2Int> expectedPath1 = {{1, 0},
-//                                          {2, 0},
-//                                          {3, 0},
-//                                          {4, 0}};
-//    EXPECT_EQ(expectedPath1.size(), res.size());
-//    for (int i = 0; i < expectedPath1.size(); ++i) {
-//        EXPECT_EQ(expectedPath1[i], res[i]);
-//    }
-//
-//
-//    uint32_t tiles2[height * width] = {
-//            0, 0, 1, 0, 0,
-//    };
-//    pathfinder::Graph testGraph2(testSize, tiles2, fToTileType);
-//    auto res2 = finder.findPath(testGraph2, testGraph2.getNodeByPos(start), testGraph2.getNodeByPos(end));
-//    EXPECT_EQ(0, res2.size());
-//
-//
-//    uint32_t tiles3[height * width] = {
-//            0, 0, 2, 0, 0,
-//    };
-//    pathfinder::Graph testGraph3(testSize, tiles3, fToTileType);
-//    auto res3 = finder.findPath(testGraph3, testGraph3.getNodeByPos(start), testGraph3.getNodeByPos(end));
-//    EXPECT_EQ(0, res3.size());
-//
-//
-//    uint32_t tiles4[height * width] = {
-//            3, 0, 2, 0, 3,
-//    };
-//    Vec2Int start2(1, 0), end2(3, 0);
-//    pathfinder::Graph testGraph4(testSize, tiles4, fToTileType);
-//    auto res4 = finder.findPath(testGraph4, testGraph4.getNodeByPos(start2), testGraph4.getNodeByPos(end2));
-//    EXPECT_EQ(0, res4.size());
-//}
-//
+TEST(PathfinderSmokeTest, pathfinderSmokeTest) {
+    const int height = 1, width = 5;
+    const cocos2d::Size testSize(width, height);
+    uint32_t tiles1[height * width] = {
+            0, 0, 0, 0, 0,
+    };
+    pathfinder::Graph testGraph(testSize, tiles1, fToTileType);
+
+
+    Vec2Int start(0, 0), end(4, 0);
+    auto res = finder.findPath(testGraph, testGraph.getNodeByPos(start), testGraph.getNodeByPos(end));
+
+    std::vector<Vec2Int> expectedPath1 = {{1, 0},
+                                          {2, 0},
+                                          {3, 0},
+                                          {4, 0}};
+    EXPECT_EQ(expectedPath1.size(), res.size());
+    for (int i = 0; i < expectedPath1.size(); ++i) {
+        EXPECT_EQ(expectedPath1[i], res[i]);
+    }
+
+
+    uint32_t tiles2[height * width] = {
+            0, 0, 1, 0, 0,
+    };
+    pathfinder::Graph testGraph2(testSize, tiles2, fToTileType);
+    auto res2 = finder.findPath(testGraph2, testGraph2.getNodeByPos(start), testGraph2.getNodeByPos(end));
+    EXPECT_EQ(0, res2.size());
+
+
+    uint32_t tiles3[height * width] = {
+            0, 0, 2, 0, 0,
+    };
+    pathfinder::Graph testGraph3(testSize, tiles3, fToTileType);
+    auto res3 = finder.findPath(testGraph3, testGraph3.getNodeByPos(start), testGraph3.getNodeByPos(end));
+    EXPECT_EQ(0, res3.size());
+
+
+    uint32_t tiles4[height * width] = {
+            3, 0, 2, 0, 3,
+    };
+    Vec2Int start2(1, 0), end2(3, 0);
+    pathfinder::Graph testGraph4(testSize, tiles4, fToTileType);
+    auto res4 = finder.findPath(testGraph4, testGraph4.getNodeByPos(start2), testGraph4.getNodeByPos(end2));
+    EXPECT_EQ(0, res4.size());
+}
+
 //TEST(PathFinderSimple1PathTest, pathfinderSimple1PathTest) {
 //    const int height = 2, width = 5;
 //    const cocos2d::Size testSize(width, height);
